49350dc905038eb6aac30b97b46747733b6e72cd
Faishal <maafaishal@gmail.com>
2024-03-10 07:48:51 +0700

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-  };
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  root: true,
+};
